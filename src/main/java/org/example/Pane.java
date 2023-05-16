ed6da182915b217b565708bb2332d807036e8c7f
Lasse <doodle.11243@gmail.com>
2023-05-16 21:15:44 +0200

    switched some names, fixed a bug while printing numbers which should be added, some variablename changes

--- src/main/java/org/example/Pane.java
+++ src/main/java/org/example/Pane.java
@@ -1,9 +1,9 @@
 package org.example;
-import java.util.Scanner;
+
 
 public class Pane {
 
-    static char[][] field = new char[3][3];
+    public char[][] pane = new char[3][3];
     int id;
 
     public Pane(int id) {
@@ -12,19 +12,17 @@ public class Pane {
         // initialize field and set all values to ' '
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                field[i][j] = ' ';
+                pane[i][j] = ' ';
             }
         }
     }
 
-    public void setSymbolAtPosition(int pane,int x, int y, Player player) {
-        if(field[x][y] != ' ') {
+    public void setSymbolAtPosition(int x, int y, Player player) {
+        if (this.pane[x - 1][y - 1] != ' ') {
             System.out.println("This field is already occupied!");
             return;
-        }
-
-        else {
-            field[x][y] = Player.symbol;
+        } else {
+            this.pane[x - 1][y - 1] = player.symbol;
             this.printPane();
             System.out.println("Symbol set!");
 
@@ -33,34 +31,35 @@ public class Pane {
 
     public void printPane(){
         //print the pane
-        System.out.println("Pane " + id+1 + ":");
+        System.out.println("Pane " + (id + 1) + ":");
         System.out.println();
         System.out.println("    |  1  |  2  |  3  |");
         for (int i = 0; i < 3; i++) {
             System.out.println("   ---------------------");
-            System.out.println("  " + (i + 1) + " |  " + field[i][0] + "  |  " + field[i][1] + "  |  " + field[i][2] + "  |");
+            System.out.println("  " + (i + 1) + " |  " + pane[i][0] + "  |  " + pane[i][1] + "  |  " + pane[i][2] + "  |");
             System.out.println("   ---------------------");
         }
     }
 
     public void printPaneWithPlaceHolder(int x, int y) {
         //print the pane but with an O at the x, y position
-        char[][] fieldCopy = new char[field.length][field[0].length];
+        char[][] copyOfCurrentPane = new char[pane.length][pane[0].length];
 
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j< 3; j++) fieldCopy[i][j] = field[i][j];
+            for (int j = 0; j < 3; j++) copyOfCurrentPane[i][j] = pane[i][j];
         }
-            if(fieldCopy[y - 1][x - 1] == ' ') fieldCopy[y - 1][x - 1] = 'O';
+        if (copyOfCurrentPane[y - 1][x - 1] == ' ') copyOfCurrentPane[y - 1][x - 1] = '?';
+        else System.out.println("This field is already occupied!");
 
 
-            System.out.println("Pane " + (id+1) + ":");
-            System.out.println();
-            System.out.println("    |  1  |  2  |  3  |");
-            for (int i = 0; i < 3; i++) {
-                System.out.println("   ---------------------");
-                System.out.println("  " + (i + 1) + " |  " + fieldCopy[i][0] + "  |  " + fieldCopy[i][1] + "  |  " + fieldCopy[i][2] + "  |");
-                System.out.println("   ---------------------");
-            }
+        System.out.println("Pane " + (id + 1) + ":");
+        System.out.println();
+        System.out.println("    |  1  |  2  |  3  |");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("   ---------------------");
+            System.out.println("  " + (i + 1) + " |  " + copyOfCurrentPane[i][0] + "  |  " + copyOfCurrentPane[i][1] + "  |  " + copyOfCurrentPane[i][2] + "  |");
+            System.out.println("   ---------------------");
+        }
 
         }
 
